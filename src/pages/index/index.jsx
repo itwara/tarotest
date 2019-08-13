61b511baa41ae2dfa81d83230830856ef95d9695
itwara <itwara@sina.com>
2019-08-13 18:16:20 +0800

    init

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,29 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text,  WebView} from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        {/* <Text>Hello world!</Text> */}
+        <WebView src='https://fang.lousiling.cn' />
+      </View>
+    )
+  }
+}
