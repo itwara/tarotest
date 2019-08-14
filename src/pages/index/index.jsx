41a309db753c96f4eb37d8a725ee7b6c153c04e3
itwara <itwara@sina.com>
2019-08-14 19:42:20 +0800

    css 布局方式默认flex，flex-direction:row;

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text,  WebView} from '@tarojs/components'
+import { View, Text,  WebView, Icon, RichText, Textarea, Switch, Image } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
@@ -20,9 +20,19 @@ export default class Index extends Component {
 
   render () {
     return (
-      <View className='index'>
+      <View className='index' style='flex-direction:column;'>
         {/* <Text>Hello world!</Text> */}
-        <WebView src='http://www.mellicapen.com' />
+        {/* <WebView src='https://fang.lousiling.cn' /> */}
+        <View  style='flex-direction:row;'>
+          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
+          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
+          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
+        </View>
+        <View  style='flex-direction:column; margin-top: 50px;'>
+          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
+          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
+          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
+        </View>
       </View>
     )
   }
