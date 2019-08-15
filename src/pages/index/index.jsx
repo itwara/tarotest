2676bba8b928bbb94717f3c722151d5becdbc5c7
itwara <itwara@sina.com>
2019-08-15 10:31:13 +0800

    格式化代码

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -24,14 +24,14 @@ export default class Index extends Component {
         {/* <Text>Hello world!</Text> */}
         {/* <WebView src='https://fang.lousiling.cn' /> */}
         <View  style='flex-direction:row;'>
-          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
-          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
-          <Text style="border: 1px solid blue;">flex-direction: row 横向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: row 横向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: row 横向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: row 横向布局</Text>
         </View>
         <View  style='flex-direction:column; margin-top: 50px;'>
-          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
-          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
-          <Text style="border: 1px solid blue;">flex-direction: column 纵向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: column 纵向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: column 纵向布局</Text>
+          <Text style='border: 1px solid blue;'>flex-direction: column 纵向布局</Text>
         </View>
       </View>
     )
